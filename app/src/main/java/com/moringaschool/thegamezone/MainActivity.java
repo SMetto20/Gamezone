64fbe3143e82e9ba5d95c709bd0377ad234eabe3
SMetto20 <smetto20@gmail.com>
2022-05-28 19:32:00 +0300

    adds various extras to the intent

--- app/src/main/java/com/moringaschool/thegamezone/MainActivity.java
+++ app/src/main/java/com/moringaschool/thegamezone/MainActivity.java
@@ -2,7 +2,9 @@ package com.moringaschool.thegamezone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -31,5 +33,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mfindSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = meditText.getText().toString();
+                String location = mLocationEditText.getText().toString();
+                String age = mageEditText.getText().toString();
+                String preference = mpreferenceEditText.getText().toString();
+                String email = memailEditText.getText().toString();
+                String password = mpasswordEditText.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra("username", username);
+                intent.putExtra("location", location);
+                intent.putExtra("age", age);
+                intent.putExtra("preference", preference);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+
+                startActivity(intent);
+            }
+
+
+        });
+
+
+
+
+
+
     }
 }
