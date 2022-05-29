f159a77f1fb39e3e13864539f93f8207068d0eb4
SMetto20 <smetto20@gmail.com>
2022-05-29 09:53:05 +0300

    adds games activity and layout

---
+++ app/src/main/java/com/moringaschool/thegamezone/GamesActivity.java
@@ -0,0 +1,13 @@
+package com.moringaschool.thegamezone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
