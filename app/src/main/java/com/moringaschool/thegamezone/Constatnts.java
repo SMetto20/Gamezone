b523601d8a3a7749d2095cae00b80540909c4f0e
SMetto20 <smetto20@gmail.com>
2022-06-03 17:05:53 +0300

    adds the game client

--- app/src/main/java/com/moringaschool/thegamezone/Constatnts.java
+++ app/src/main/java/com/moringaschool/thegamezone/Constatnts.java
@@ -1,5 +1,5 @@
 package com.moringaschool.thegamezone;
 
 public class Constatnts {
-    public static final String GAME_BASE_URL = "https://www.freetogame.com/api/games";
+    public static final String GAME_BASE_URL = "https://www.freetogame.com";
 }
--- app/src/main/java/interfaces/GameApi.java
+++ app/src/main/java/interfaces/GameApi.java
@@ -1,4 +1,11 @@
 package interfaces;
 
+import com.moringaschool.thegamezone.GamesListResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface GameApi {
+    @GET("/api/games")
+    Call<GamesListResponse> getGames();
 }
