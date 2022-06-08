2f025748d1bd90dd9321406107bedd7bb54540be
SMetto20 <smetto20@gmail.com>
2022-06-08 12:52:43 +0300

    add sign in fragment layout

--- app/src/main/java/com/moringaschool/thegamezone/SigninDialogFragment.java
+++ app/src/main/java/com/moringaschool/thegamezone/SigninDialogFragment.java
@@ -1,4 +1,16 @@
 package com.moringaschool.thegamezone;
 
-public class SigninDialogFragment {
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SigninDialogFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.signin_dialog, container, false);
+        getDialog().setTitle("Sign in");
+        return rootView;
+    }
 }
