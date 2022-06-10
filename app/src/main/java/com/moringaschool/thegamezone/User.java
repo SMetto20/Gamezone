cd44fd8346a9b6b57c08320b9ede46a49414c3bf
SMetto20 <smetto20@gmail.com>
2022-06-10 13:49:04 +0300

    add constructor, getters and setters for user class

--- app/src/main/java/com/moringaschool/thegamezone/User.java
+++ app/src/main/java/com/moringaschool/thegamezone/User.java
@@ -1,4 +1,63 @@
 package com.moringaschool.thegamezone;
 
 public class User {
+    String name, location , preference,email, password;
+    int age;
+
+    public User(String name, String location, String preference, String email, String password, int age) {
+        this.name = name;
+        this.location = location;
+        this.preference = preference;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
