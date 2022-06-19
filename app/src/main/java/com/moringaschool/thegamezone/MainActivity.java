0a41bc257b4497ddcbae94cffb1d431f5d85d5b9
SMetto20 <smetto20@gmail.com>
2022-06-19 09:33:51 +0300

    add animation resource file

--- app/src/main/java/com/moringaschool/thegamezone/MainActivity.java
+++ app/src/main/java/com/moringaschool/thegamezone/MainActivity.java
@@ -6,16 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.util.Pair;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.startedbutton)
     Button mStartedButton;
 
+    @BindView(R.id.imageView3)
+    ImageView mimageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +50,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(intent);
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View,String>(mimageView,"Kratos");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(intent, options.toBundle());
 
             }
             });
